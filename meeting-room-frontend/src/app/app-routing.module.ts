73919c5f0bfaa57ddd815b86452bd8702e57a69c
Jeevan C R <145118797+jeencr@users.noreply.github.com>
2026-05-28 13:09:49 +0530

    Angular file structure defined and starting the services

--- meeting-room-frontend/src/app/app-routing.module.ts
+++ meeting-room-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ManageBookingsComponent } from './pages/admin/manage-bookings/manage-bookings.component';
+import { ManageRoomsComponent } from './pages/admin/manage-rooms/manage-rooms.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { MyBookingsComponent } from './pages/customer/my-bookings/my-bookings.component';
+import { CreateBookingComponent } from './pages/customer/create-booking/create-booking.component';
+import { RoomListComponent } from './pages/customer/room-list/room-list.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AdminGuard } from './guards/admin.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '',redirectTo: 'login',pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'rooms', component: RoomListComponent,canActivate: [AuthGuard]},
+    {path: 'create-booking', component: CreateBookingComponent,canActivate: [AuthGuard]},
+    {path: 'my-bookings', component: MyBookingsComponent,canActivate: [AuthGuard]},
+    {path: 'dashboard', component: DashboardComponent,canActivate: [AdminGuard]},
+    {path: 'manage-rooms', component: ManageRoomsComponent,canActivate: [AdminGuard]},
+    {path: 'manage-bookings', component: ManageBookingsComponent,canActivate: [AdminGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- meeting-room-frontend/src/app/app.module.ts
+++ meeting-room-frontend/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { RoomListComponent } from './pages/customer/room-list/room-list.component';
+import { CreateBookingComponent } from './pages/customer/create-booking/create-booking.component';
+import { MyBookingsComponent } from './pages/customer/my-bookings/my-bookings.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ManageRoomsComponent } from './pages/admin/manage-rooms/manage-rooms.component';
+import { ManageBookingsComponent } from './pages/admin/manage-bookings/manage-bookings.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RoomListComponent,
+    CreateBookingComponent,
+    MyBookingsComponent,
+    DashboardComponent,
+    ManageRoomsComponent,
+    ManageBookingsComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
