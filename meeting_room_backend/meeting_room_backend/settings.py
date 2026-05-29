321c3d852b6f3b7b5b130428bce960cb5b1c5a45
Jeevan C R <145118797+jeencr@users.noreply.github.com>
2026-05-29 10:18:42 +0530

    preparing to deploy

--- meeting_room_backend/meeting_room_backend/settings.py
+++ meeting_room_backend/meeting_room_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%$3js-r$w4_8pi9e(4k&gdwo=g^7%%9&(u18x^lno72_xpdeft
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
